22f02e45dd03001445b3226c7b868c78649d0a81
Lucas Sachet <lucasachet@gmail.com>
2022-03-01 22:45:03 -0300

    add exeption to signup

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,26 +1,36 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { AuthInterface } from './interface';
 import * as argon from 'argon2';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
 
 @Injectable()
 export class AuthService {
     constructor(private prisma: PrismaService) {}
-  login() {}
+    login() {}
 
     async signUp(dto: AuthInterface) {
-        //generate password
-        const hash = await argon.hash(dto.password)
-        //save the new user in the db
-        const user = await this.prisma.user.create({
-            data: {
-                email: dto.email,
-                hash
-            },
-        })
-        delete user.hash;
-        //return the saved user
-        return user
+        try {
+            //generate password
+            const hash = await argon.hash(dto.password)
+            //save the new user in the db
+            const user = await this.prisma.user.create({
+                data: {
+                    email: dto.email,
+                    hash
+                },
+            })
+            delete user.hash;
+            //return the saved user
+            return user
+        } catch (error) {
+            if (error instanceof PrismaClientKnownRequestError) {
+                if (error.code === 'P2002') {
+                    throw new ForbiddenException('Credentials taken')
+                }
+                throw error;
+            }
+        }
      }
     
     signIn() {

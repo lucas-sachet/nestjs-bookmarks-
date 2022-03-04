650e4a05c0024d09b72577957a815c05d4fb0e68
Lucas Sachet <lucasachet@gmail.com>
2022-03-03 21:49:37 -0300

    add signin

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -6,34 +6,47 @@ import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
 
 @Injectable()
 export class AuthService {
-    constructor(private prisma: PrismaService) {}
-    login() {}
+    constructor(private prisma: PrismaService) { }
+    login() { }
 
     async signUp(dto: AuthInterface) {
         try {
             //generate password
-            const hash = await argon.hash(dto.password)
+            const hash = await argon.hash(dto.password);
             //save the new user in the db
             const user = await this.prisma.user.create({
                 data: {
                     email: dto.email,
-                    hash
+                    hash,
                 },
-            })
+            });
             delete user.hash;
             //return the saved user
-            return user
+            return user;
         } catch (error) {
             if (error instanceof PrismaClientKnownRequestError) {
                 if (error.code === 'P2002') {
-                    throw new ForbiddenException('Credentials taken')
+                    throw new ForbiddenException('Credentials taken');
                 }
                 throw error;
             }
         }
-     }
-    
-    signIn() {
-        return { msg: 'I have signed in'}
+    }
+
+    async signIn(dto: AuthInterface) {
+        //find user by email
+        const user = await this.prisma.user.findUnique({
+            where: {
+                email: dto.email,
+            }
+        })
+        if (!user) throw new ForbiddenException('Credentials incorrect')
+        
+        //compare password
+        const pwMatches = await argon.verify(user.hash, dto.password)
+        if(!pwMatches) throw new ForbiddenException('Credentials incorrect')
+        
+        delete user.hash;
+        return user;
     }
 }
